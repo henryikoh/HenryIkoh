e3e84665318d9ec4c23cc3d750a03edd6297200a
Henry Ikoh <henryikoh@Henrys-MacBook-Air.local>
2024-02-19 13:10:31 +0100

    kinda stable added some services

--- components/ServicesTab.vue
+++ components/ServicesTab.vue
@@ -4,7 +4,7 @@
       <ul  class=" gap-8 md:gap-6 grid md:grid-cols-3">
                     <li >
                         <div>
-                            <h3 class="  text-2xl text-white">$1000</h3>
+                            <h3 class="  text-2xl text-white">$1500</h3>
                             <h1 class="text-4xl font-bold dark:text-white pb-9">   Product Strategy</h1>
                          
                         </div>
